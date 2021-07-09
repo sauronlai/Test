04e6ba5a73598d9e041957904dd9d2fa583563b0
sauronlai <sauronlai@SauronLaideMacBook-Air.local>
2021-07-09 12:14:39 +0800

    calculation_2

--- run_3.py
+++ run_3.py
@@ -1,18 +1,31 @@
-try:
-	a = int(input('請輸入第一位數字：'))
-	b = int(input('請輸入第二位數字：'))
-	c = int(input('請選擇何種運算符號？\n1.加, 2.減, 3.乘, 4.除'))
-	if c == 1:
-		print('{} 和 {} 相加後的答案為：{}'.format(a, b, a+b))
-	elif c == 2:
+while True:
+	try:
+		a = int(input('請輸入第一位數字：'))
+		b = int(input('請輸入第二位數字：'))
+		c = int(input('請選擇何種運算符號？\n1.加, 2.減, 3.乘, 4.除'))
+		if c == 1:
+			print('{} 和 {} 相加後的答案為：{}'.format(a, b, a+b))
+			d = input('要繼續嗎？按Q離開')
+			if d == 'q' or d == 'Q':
+				break
+		elif c == 2:
 			print('{} 和 {} 相減後的答案為：{}'.format(a, b, a-b))
-	elif c == 3:
+			d = input('要繼續嗎？按Q離開')
+			if d == 'q' or d == 'Q':
+				break
+		elif c == 3:
 			print('{} 和 {} 相乘後的答案為：{}'.format(a, b, a*b))
-	elif c == 4:
+			d = input('要繼續嗎？按Q離開')
+			if d == 'q' or d == 'Q':
+				break
+		elif c == 4:
 			print('{} 和 {} 相除後的答案為：{}'.format(a, b, a/b))
-	else:
-		print('輸入錯誤')
-except ValueError:
-    print("輸入格式有誤")
-except:
-    print("程式出現其它異常")
+			d = input('要繼續嗎？按Q離開')
+			if d == 'q' or d == 'Q':
+				break
+		else:
+			print('輸入錯誤')
+	except ValueError:
+		print("輸入格式有誤")
+	except:
+		print("程式出現其它異常")
---
+++ run_4.py
@@ -0,0 +1,1 @@
+print('Bon Voyage!!')
